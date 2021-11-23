e23a73ecb9ce4a3680231de12cce4c35f14b81bc
99yaswanth <yashwanth24k@gmail.com>
2021-11-23 17:46:47 +0530

    install

--- Jenkinsfile_tomcat.groovy
+++ Jenkinsfile_tomcat.groovy
@@ -15,6 +15,7 @@ pipeline{
                 sh """
                 scp -o StrictHostKeyChecking=no -i /tmp/nvirginia.pem /tmp/tomcatinstallation.sh ec2-user@${SERVERIP}:/tmp/
                 ssh -o StrictHostKeyChecking=no -i /tmp/nvirginia.pem ec2-user@${SERVERIP} "ls -l /tmp/"
+                ssh -o StrictHostKeyChecking=no -i /tmp/nvirginia.pem ec2-user@${SERVERIP} "sudo bash /tmp/tomcatinstallation.sh"
                 """
             }
         }
